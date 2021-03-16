64c143a31a8cf36fcb5d5986f3ff8e4b9a7dba4b
kenpachi <itwabi@gmail.com>
2021-03-16 15:41:45 +0200

    flattening the selected OrgUnit node, added modals to the layout

--- src/MainForm.js
+++ src/MainForm.js
@@ -8,7 +8,7 @@ import {
     MDBCardText,
     MDBCardTitle,
     MDBCol,
-    MDBContainer,
+    MDBContainer, MDBModal, MDBModalBody, MDBModalFooter, MDBModalHeader,
     MDBRow,
 } from "mdbreact";
 import {Button, Dropdown, Menu, TreeSelect} from "antd";
@@ -20,35 +20,43 @@ import Header from "@dhis2/d2-ui-header-bar"
 
 const MainForm = (props) => {
 
-    var periods = ["Choose By","Week", "Month"];
     var orgUnitFilters = ["Filter By", "Markets"];
+    const basicAuth = "Basic " + btoa("ahmed:Atwabi@20");
 
     const [showLoading, setShowLoading] = useState(false);
     const [orgUnits, setOrgUnits] = useState([]);
     const [programs, setPrograms] = useState([]);
-    const [markets, setMarkets] = useState([]);
     const [searchValue, setSearchValue] = useState();
-    const [selectedOrgUnit, setSelectedOrgUnit] = useState(null);
     const [selectedProgram, setSelectedProgram] = useState(null);
-    const [headerNames, setHeaderNames] = useState([]);
     const [orgFilter, setOrgFilter] = useState(orgUnitFilters[0]);
     const [choseFilter, setChoseFilter] = useState(false);
     const [treeMarkets, setTreeMarkets] = useState(null);
     const [treeValue, setTreeValue] = useState();
     const [flattenedUnits, setFlattenedUnits] = useState([]);
     const [D2, setD2] = useState();
+    const [modal, setModal] = useState(false);
+    const [alertModal, setAlertModal] = useState(false);
+    const [message, setMessage] = useState("");
+    const [messageBody, setMessageBody] = useState("");
+
     getInstance().then(d2 =>{
         setD2(d2);
-    })
+    });
+
+    const toggle = () => {
+        setModal(!modal)
+    }
+
+    const toggleAlert = () => {
+        setAlertModal(!alertModal);
+    }
 
     useEffect(() => {
         setOrgUnits(props.organizationalUnits);
         setPrograms(props.programs);
-        setMarkets(props.marketOrgUnits);
         setTreeMarkets(props.treeMarkets);
 
-
-    },[headerNames, props.organizationalUnits, props.programs, props.d2, props.marketOrgUnits, props.treeMarkets]);
+    },[props.organizationalUnits, props.programs, props.d2, props.marketOrgUnits, props.treeMarkets]);
 
 
     const handle = (value, label, extra) => {
@@ -56,21 +64,25 @@ const MainForm = (props) => {
     };
 
     const onSelect = (value, node) => {
-        //setOrgUnit(selectedOrgUnit => [...selectedOrgUnit, node]);
-        setSelectedOrgUnit(node);
-        //console.log(node);
+        //setSelectedOrgUnit(node);
 
-        let extractChildren = x => x.children;
-        let flat = flatten(extractChildren(node), extractChildren, node.level, node.parent)
-            .map(x => delete x.children && x);
-        console.log(flat)
-        setFlattenedUnits(flat);
-
+        var children = extractChildren(node)
+        var tempArray = [];
+        if(children === undefined){
+            tempArray.push(node);
+            setFlattenedUnits(tempArray)
+        } else {
+            let flat = flatten(extractChildren(node), extractChildren, node.level, node.parent)
+                .map(x => delete x.children && x);
+            //console.log(flat)
+            setFlattenedUnits(flat);
+        }
     };
 
+    let extractChildren = x => x.children;
     let flatten = (children, getChildren, level, parent) => Array.prototype.concat.apply(
-        children.map(x => ({ ...x, level: level || 1, parent: parent || null })),
-        children.map(x => flatten(getChildren(x) || [], getChildren, (level || 1) + 1, x.id))
+        children && children.map(x => ({ ...x, level: level || 1, parent: parent || null })),
+        children && children.map(x => flatten(getChildren(x) || [], getChildren, (level || 1) + 1, x.id))
     );
 
     const handleTree = (value, label, extra) => {
@@ -80,81 +92,82 @@ const MainForm = (props) => {
 
     const onSelectTree = (value, node) => {
         //setOrgUnit(selectedOrgUnit => [...selectedOrgUnit, node]);
-        setSelectedOrgUnit(node);
+        //setSelectedOrgUnit(node);
         console.log(node);
 
-        //console.log(flatten())
-        let extractChildren = x => x.children;
-        let flat = flatten(extractChildren(node), extractChildren, node.level, node.parent)
-            .map(x => delete x.children && x);
-        console.log(flat)
-        setFlattenedUnits(flat);
+        var children = extractChildren(node);
+
+        if(children === undefined){
+            setFlattenedUnits([node]);
+        } else {
+            let flat = flatten(extractChildren(node), extractChildren, node.level, node.parent)
+                .map(x => delete x.children && x);
+            //console.log(flat)
+            setFlattenedUnits(flat);
+        }
     };
-
-    const handleDeletion = () => {
-        var progID = selectedProgram.id;
-
-
-
-    }
-
-
 
     const handleProgram = selectedOption => {
-
         console.log(selectedOption);
-
-        getInstance().then((d2) => {
-            const endpoint = `programs/${selectedOption.id}.json?fields=id,name,trackedEntityType[id,name]`;
-            d2.Api.getApi().get(endpoint)
-                .then((response) => {
-                    console.log(response.trackedEntityType.name);
-                    selectedOption.entityTypeName = response.trackedEntityType.name;
-                    setSelectedProgram(selectedOption);
-                })
-                .catch((error) => {
-                    console.log("An error occurred: " + error);
-                    alert("An error occurred: " + error);
-                });
-        });
+        setSelectedProgram(selectedOption);
 
     };
 
-    const functionWithPromise = eventItem => { //a function that returns a promise
-        getInstance().then((d2) => {
-            eventItem.dataValues.map((dataValue) => {
-                const endpoint = `dataElements/${dataValue.dataElement}.json`;
-                d2.Api.getApi().get(endpoint)
-                    .then((response) => {
-                        //console.log(dataValue);
-                        //console.log(dataValue.dataElement);
-                        var data = {"id" : response.id, "name" : response.displayFormName,
-                            "trackedInstance" : eventItem.trackedEntityInstance};
-                        dataValue.displayName = response.displayFormName;
+    const deleteEnrolment = (orgUnitID, programID) => {
 
-                        setHeaderNames(headerName => [...headerName, data]);
-                    })
-                    .catch((error) => {
-                        console.log("An error occurred: " + error);
-                        alert("An error occurred: " + error);
-                    });
-            });
+        fetch(`https://covmw.com/namistest/api/enrollments.json?ou=${orgUnitID}&program=${programID}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization' : basicAuth,
+                'Content-type': 'application/json',
+            },
+            credentials: "include"
 
-            //dataOver.dataValues = tempArray;
-            //setHeaderNames(headerName => [...headerName, dataOver]);
+        })
+            .then(response => response.json())
+            .then((result) => {
+                console.log(result);
+                setMessage("Success");
+                setMessageBody("The enrollments for the chosen program and orgUnits were successfully deleted")
+            })
+            .catch((error) => {
+            setMessage("Error");
+            setMessageBody("Unable to delete due to an error: " + error)
+        })
+    }
 
-        });
+    const functionWithPromise = orgUnit => { //a function that returns a promise
 
-
-        return eventItem;
+        deleteEnrolment(orgUnit.id, selectedProgram.id);
+        return message;
     }
 
     const anAsyncFunction = async item => {
         return functionWithPromise(item)
     }
 
-    const getData = async (list) => {
+    const deleteData = async (list) => {
         return await Promise.all(list.map(item => anAsyncFunction(item)))
+    }
+
+    const handleDeletion = () => {
+        toggle();
+        setShowLoading(true);
+        //var progID = selectedProgram.id;
+        console.log(flattenedUnits)
+        //console.log(progID);
+
+        if(flattenedUnits.length !== 0  && selectedProgram !== null){
+            deleteData(flattenedUnits).then((r) =>{
+                setShowLoading(false);
+            }).catch((err) => {
+                console.log("an error occurred: " + err);
+            });
+        } else {
+            console.log("things are null");
+        }
+
+
     }
 
 
@@ -163,13 +176,13 @@ const MainForm = (props) => {
         if(value === "Markets"){
             setChoseFilter(true);
             setFlattenedUnits([]);
-            setSelectedOrgUnit(null)
+            //setSelectedOrgUnit(null)
             setSearchValue(null);
             setTreeValue(null);
         } else {
             setChoseFilter(false);
             setFlattenedUnits([]);
-            setSelectedOrgUnit(null)
+            //setSelectedOrgUnit(null)
             setSearchValue(null);
             setTreeValue(null);
         }
@@ -205,6 +218,29 @@ const MainForm = (props) => {
                                 {programs.length == 0 ? <div className="spinner-border mx-2 indigo-text spinner-border-sm" role="status">
                                     <span className="sr-only">Loading...</span>
                                 </div> : null}
+
+                                <MDBContainer>
+                                    <MDBModal isOpen={modal} toggle={toggle} centered>
+                                        <MDBModalHeader toggle={toggle}>Confirmation</MDBModalHeader>
+                                        <MDBModalBody>
+                                            All the enrollments for the chosen orgUnit(and it's children) will be deleted.
+                                            Are you sure you want to delete?
+                                        </MDBModalBody>
+                                        <MDBModalFooter>
+                                            <MDBBtn color="secondary" className="mx-1" onClick={toggle}>Cancel</MDBBtn>
+                                            <MDBBtn color="primary" className="mx-1" onClick={handleDeletion}>Delete</MDBBtn>
+                                        </MDBModalFooter>
+                                    </MDBModal>
+                                </MDBContainer>
+
+                                <MDBContainer>
+                                    <MDBModal isOpen={alertModal} toggle={toggleAlert} centered>
+                                        <MDBModalHeader toggle={toggleAlert}>{message}</MDBModalHeader>
+                                        <MDBModalBody>
+                                            {messageBody}
+                                        </MDBModalBody>
+                                    </MDBModal>
+                                </MDBContainer>
 
                                 <hr/>
 
@@ -275,7 +311,7 @@ const MainForm = (props) => {
 
                                 <div className="text-center py-4 mt-2">
 
-                                    <MDBBtn color="cyan" className="text-white">
+                                    <MDBBtn color="cyan" className="text-white" onClick={toggle}>
                                         Delete Enrolments{showLoading ? <div className="spinner-border mx-2 text-white spinner-border-sm" role="status">
                                         <span className="sr-only">Loading...</span>
                                     </div> : null}
